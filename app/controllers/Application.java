d3d6dd195f78007eee2a3409c40a14fe39479b0d
kenichi_shintome <kshintome@say-tech.co.jp>
2014-12-24 17:06:56 +0900

    空のデータベース処理

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,34 +1,18 @@
 package controllers;
 
+import java.util.List;
+
+import models.Message;
 import play.*;
 import play.data.*;
-import static play.data.Form.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
-    // Form用の内部クラス
-    public static class SampleForm{
-        public String message;
-    }
-        
-    // ルートにアクセスした際のAction
-    public static Result index(){
-        return ok(index.render("何か書いて！", new Form(SampleForm.class)));
-    }
-
-    // /sendにアクセスした際のAction
-    public static Result send() {
-        Form<SampleForm> f = form(SampleForm.class).bindFromRequest();
-        if(!f.hasErrors()){
-            SampleForm data = f.get();
-            String msg = "you typed: " + data.message;
-            return ok(index.render(msg,f));
-        } else {
-            return badRequest(index.render("ERROR", form(SampleForm.class)));
-        }
-    }
-
+	// ルートにアクセスした際のAction
+	public static Result index() {
+		List<Message> datas = Message.find.all();
+		return ok(index.render("データベースのサンプル", datas));
+	}
 }
