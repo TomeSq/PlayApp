31125d73992c31b6f1524da0edc71408ff064ba5
ktome <ken@ken-XPS-M1330>
2014-12-28 23:31:33 +0900

    データベース初期

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,23 +9,8 @@ import views.html.*;
 
 public class Application extends Controller {
 
-	// Form 内部クラス
-	public static class SampleForm{
-		public String message;
-	}
-
     public static Result index() {
-        return ok(index.render("何か書いて。", new Form(SampleForm.class)));
+        return ok(index.render("データベースのサンプル"));
     }
     
-    public static Result send(){
-    	Form<SampleForm> f = form(SampleForm.class).bindFromRequest();
-    	if(!f.hasErrors()){
-    		SampleForm data = f.get();
-    		String msg = "you typed:" + data.message;
-    		return ok(index.render(msg,f));
-    	} else {
-    		return badRequest(index.render("ERROR", form(SampleForm.class)));
-    	}
-    }
 }
