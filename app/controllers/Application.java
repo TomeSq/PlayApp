694cb0e3d105c99b6d117c16b7b831cf28460288
ktome <ken@ken-XPS-M1330>
2014-12-28 21:34:19 +0900

    入力フォームテスト

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,31 @@
 package controllers;
 
 import play.*;
+import play.data.*;
+import static play.data.Form.*;
 import play.mvc.*;
 
 import views.html.*;
 
 public class Application extends Controller {
 
+	// Form 内部クラス
+	public static class SampleForm{
+		public String message;
+	}
+
     public static Result index() {
-        return ok(index.render(
-        		"これは最初の値です。",
-        		"これは真ん中の引数です。",
-        		"これは最後のテキストです。"));
+        return ok(index.render("何か書いて。", new Form(SampleForm.class)));
+    }
+    
+    public static Result send(){
+    	Form<SampleForm> f = form(SampleForm.class).bindFromRequest();
+    	if(!f.hasErrors()){
+    		SampleForm data = f.get();
+    		String msg = "you typed:" + data.message;
+    		return ok(index.render(msg,f));
+    	} else {
+    		return badRequest(index.render("ERROR", form(SampleForm.class)));
+    	}
     }
 }
