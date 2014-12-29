3c2e17f7ef0ceed0a262c509b00ecb89cd6cb982
ktome <ken@ken-XPS-M1330>
2014-12-29 15:40:06 +0900

    CRUDの実装

--- app/models/Message.java
+++ app/models/Message.java
@@ -5,17 +5,27 @@ import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import com.avaje.ebean.annotation.*;
+
 import play.db.ebean.*;
 import play.data.validation.*;
+import play.data.validation.Constraints.*;
 
 @Entity
 public class Message extends Model {
 
 	@Id
 	public Long id;
+	
+	@Required
 	public String name;
+
 	public String mail;
+
+	@Required
 	public String message;
+
+	@CreatedTimestamp
 	public Date postdate;
 	
 	public static Finder<Long, Message> find = 
