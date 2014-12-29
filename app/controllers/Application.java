5f4c1fd15f9e296cce3e8c69c59a534796b1a5bc
ktome <ken@ken-XPS-M1330>
2014-12-29 14:22:45 +0900

    データ入力フォーム作成

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,35 @@
 package controllers;
 
+import java.util.List;
+
+import models.Message;
 import play.*;
 import play.data.*;
 import static play.data.Form.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("データベースのサンプル"));
+    	List<Message> datas = Message.find.all();
+    	return ok(index.render("データベースのサンプル", datas));
+    }
+    
+    public static Result add(){
+    	Form<Message> f = new Form(Message.class);
+    	return ok(add.render("投稿フォーム", f));
+    }
+    
+    public static Result create(){
+    	Form<Message> f = new Form(Message.class).bindFromRequest();
+    	if(!f.hasErrors()){
+    		Message data = f.get();
+    		data.save();
+    		return redirect("/");
+    	} else {
+    		return badRequest(add.render("Error", f));
+    	}
     }
     
 }
--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := """PlayTest"""
+name := """PlayApp"""
 
 version := "1.0-SNAPSHOT"
 
