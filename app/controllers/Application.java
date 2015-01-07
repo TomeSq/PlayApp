0f7abd51a7e1913635f712ee55ce84d364e59a6b
kenichi_shintome <kshintome@say-tech.co.jp>
2015-01-07 14:52:26 +0900

    いろいろなForm入力

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,22 +1,55 @@
 package controllers;
 
-import java.util.Map;
+import java.util.*;
 
-import models.*;
+import play.*;
+import play.data.*;
+import play.mvc.*;
+
 import views.html.*;
-import play.mvc.Controller;
-import play.mvc.Result;
 
 public class Application extends Controller {
+	// フォーム管理用クラス
+	public static class SampleForm{
+		public int number;
+		public String input;
+		public String pass;
+		public boolean check;
+		public String radio;
+		public String sel;
+		public String area;
+		public Date date;
+	}
+
 	// ルートにアクセスした際のAction
 	public static Result index(){
-		String method = request().method();
-		if("GET".equals(method)){
-			return ok(index.render("please type:"));
+		SampleForm sf = new SampleForm();
+		sf.radio = "windows";
+		sf.check = true;
+		sf.input = "default value";
+		sf.sel = "uk";
+		Form<SampleForm> form = new Form(SampleForm.class).fill(sf);
+
+		return ok(index.render("please set form.", form));
+	}
+
+	// POST送信された時のAction
+	public static Result send(){
+		Form<SampleForm> f = new Form(SampleForm.class).bindFromRequest();
+		if(!f.hasErrors()){
+			SampleForm sf = f.get();
+			String res = "value: ";
+			res += "number=" + sf.number
+				+ "input=" + sf.input
+				+ ", pass=" + sf.pass
+				+ ", check=" + sf.check
+				+ ", radio=" + sf.radio
+				+ ", sel=" + sf.sel
+				+ ", area=" + sf.area
+				+ ", date=" + sf.date;
+			return ok(index.render(res, f));
 		} else {
-			Map<String, String[]> form = request().body().asFormUrlEncoded();
-			String[] input = form.get("input");
-			return ok(index.render("posted:" + input[0]));
+			return badRequest(index.render("ERROR", f));
 		}
 	}
 }
