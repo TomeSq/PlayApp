d74f1fd695f6ef4620d2d462b106f623d5e53353
kenichi_shintome <kshintome@say-tech.co.jp>
2015-01-07 14:01:05 +0900

    標準フォームからのPOST,GET

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,22 @@
 package controllers;
 
+import java.util.Map;
+
 import models.*;
 import views.html.*;
-
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
 	// ルートにアクセスした際のAction
-	public static Result index(String msg, int id){
-		return ok(index.render("引数は、" + msg + "," + id + "です。"));
+	public static Result index(){
+		String method = request().method();
+		if("GET".equals(method)){
+			return ok(index.render("please type:"));
+		} else {
+			Map<String, String[]> form = request().body().asFormUrlEncoded();
+			String[] input = form.get("input");
+			return ok(index.render("posted:" + input[0]));
+		}
 	}
 }
