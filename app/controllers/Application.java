ea764c4a36d57aedb08c5b875f8bf3e60720ce18
kenichi_shintome <kshintome@say-tech.co.jp>
2015-01-07 15:21:11 +0900

    入力フォーム繰り返し例

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,25 +11,12 @@ import views.html.*;
 public class Application extends Controller {
 	// フォーム管理用クラス
 	public static class SampleForm{
-		public int number;
-		public String input;
-		public String pass;
-		public boolean check;
-		public String radio;
-		public String sel;
-		public String area;
-		public Date date;
+		public List<String> inputs;
 	}
 
 	// ルートにアクセスした際のAction
 	public static Result index(){
-		SampleForm sf = new SampleForm();
-		sf.radio = "windows";
-		sf.check = true;
-		sf.input = "default value";
-		sf.sel = "uk";
-		Form<SampleForm> form = new Form(SampleForm.class).fill(sf);
-
+		Form<SampleForm> form = new Form(SampleForm.class);
 		return ok(index.render("please set form.", form));
 	}
 
@@ -39,14 +26,10 @@ public class Application extends Controller {
 		if(!f.hasErrors()){
 			SampleForm sf = f.get();
 			String res = "value: ";
-			res += "number=" + sf.number
-				+ "input=" + sf.input
-				+ ", pass=" + sf.pass
-				+ ", check=" + sf.check
-				+ ", radio=" + sf.radio
-				+ ", sel=" + sf.sel
-				+ ", area=" + sf.area
-				+ ", date=" + sf.date;
+			for(String s : sf.inputs){
+				res += " " + s;
+			}
+			sf.inputs.add("");
 			return ok(index.render(res, f));
 		} else {
 			return badRequest(index.render("ERROR", f));
