93832cedbbd63916f08473d56c959082c11ca9e1
kenichi_shintome <kshintome@say-tech.co.jp>
2015-01-07 12:01:24 +0900

    ControllerだけでHTML表示してみた

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,64 +1,13 @@
 package controllers;
 
-import java.util.List;
-
+import models.*;
 import views.html.*;
-import models.Member;
-import models.Message;
-import play.data.Form;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
-
-	// ルートにアクセスした際のAction
-	public static Result index() {
-		List<Message> datas  = Message.find.all();
-		List<Member>  datas2 = Member.find.all();
-		return ok(index.render("データベースのサンプル", datas, datas2));
-	}
-
-	// Message Action =================
-	// 新規投稿フォームのAction
-	public static Result add() {
-		Form<Message> f = new Form(Message.class);
-		return ok(add.render("投稿フォーム", f));
-	}
-
-	// /createにアクセスした際のAction
-	public static Result create() {
-		Form<Message> f = new Form(Message.class).bindFromRequest();
-		if (!f.hasErrors()) {
-			Message data = f.get();
-			String[] names = data.name.split(",");
-			for(String nm : names){
-				Member m = Member.findByName(nm);
-				data.members.add(m);
-				m.messages.add(data);
-			}
-			data.save();
-			return redirect("/");
-		} else {
-			return badRequest(add.render("ERROR", f));
-		}
-	}
-
-	// Membet Action =================
-	// メンバー作成フォームのAction
-	public static Result add2() {
-		Form<Member> f = new Form(Member.class);
-		return ok(add2.render("メンバー登録フォーム", f));
-	}
-
-	// /create2にアクセスした際のAction
-	public static Result create2() {
-		Form<Member> f = new Form(Member.class).bindFromRequest();
-		if (!f.hasErrors()) {
-			Member data = f.get();
-			data.save();
-			return redirect("/");
-		} else {
-			return badRequest(add2.render("ERROR", f));
-		}
+	public static Result index(){
+		return ok("<HTML> <body> <h1>HELLO!</h1> <p>This is test.</p> </body> </HTML>").as("text/html");
 	}
 }
