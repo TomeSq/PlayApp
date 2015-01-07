5a5e582363fb36098cf938bc31b30df1919b1bba
kenichi_shintome <kshintome@say-tech.co.jp>
2015-01-07 15:59:24 +0900

    情報を入力できるように修正

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -19,4 +19,43 @@ public class Application extends Controller {
 		List<Message> msgs = Message.find.all();
 		return ok(index.render("please set form.", msgs));
 	}
+	// Message Action =================
+	// 新規投稿フォームのAction
+	public static Result add() {
+		Form<Message> f = new Form(Message.class);
+		return ok(add.render("投稿フォーム", f));
+	}
+
+	// /createにアクセスした際のAction
+	public static Result create() {
+		Form<Message> f = new Form(Message.class).bindFromRequest();
+		if (!f.hasErrors()) {
+			Message data = f.get();
+			data.member = Member.findByName(data.name);
+			data.save();
+			return redirect("/");
+		} else {
+			return badRequest(add.render("ERROR", f));
+		}
+	}
+
+	// Membet Action =================
+	// メンバー作成フォームのAction
+	public static Result add2() {
+		Form<Member> f = new Form(Member.class);
+		return ok(add2.render("メンバー登録フォーム", f));
+	}
+
+	// /create2にアクセスした際のAction
+	public static Result create2() {
+		Form<Member> f = new Form(Member.class).bindFromRequest();
+		if (!f.hasErrors()) {
+			Member data = f.get();
+			data.save();
+			return redirect("/");
+		} else {
+			return badRequest(add2.render("ERROR", f));
+		}
+	}
+
 }
